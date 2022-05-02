b3bf166c1527d71d8348c8ffeda31889179864bd
BiasConch <joelrasinski@gmail.com>
2022-05-01 20:43:46 -0500

    Write to file unique file

--- FIle/Word_File.py
+++ FIle/Word_File.py
@@ -1,14 +1,22 @@
 import os
+import shutil
 
 def Word_File():
 
-    path = '/home/malware/Desktop/InFeCtEd/Word'
+    path = '/home/malware/Desktop/InFeCtEd/Word/'
 
 
     for entry in os.listdir(path):
-        os.chdir(path)
-        md5sum = os.system("md5sum" + entry)
-        file_check = os.system("file" + entry)
-        oleid = os.system("oleid" + entry)
-        if (oleid.find('file contains VBA macros') != -1):
-            olevba = os.system("olevba" + entry)
+        if os.path.isfile(os.path.join(path, entry)):
+            os.mkdir(path + entry)
+            os.system("touch " + entry + ".txt" )
+            os.chdir(path)
+            md5sum = os.system("md5sum " + entry)
+            file_check = os.system("file " + entry)
+            oleid = os.system("oleid " + entry)
+            
+            if (oleid.find('file contains VBA macros') != -1):
+                olevba = os.system("olevba " + entry)
+            
+            
+            shutil.move(path + entry, '/home/malware/Desktop/InFeCtEd/Word/' + entry)
